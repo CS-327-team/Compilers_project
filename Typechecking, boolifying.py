3fc7b59a287e8f4e8b7e55e2a7f992643944c07e
Utkarsh Mittal <97827155+UtkarshMitta@users.noreply.github.com>
2023-02-20 03:51:30 +0530

    Add lexer

--- Typechecking, boolifying.py
+++ Typechecking, boolifying.py
@@ -1,6 +1,150 @@
 from dataclasses import dataclass
 from fractions import Fraction
-from typing import Union, Mapping
+from typing import Union, Mapping, Literal
+
+digit_list = "1234567890"
+alphabet_list = "ABCDEFGHIJKLOMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+
+@dataclass
+class EndOfStream(Exception):
+    pass
+
+
+@dataclass
+class Stream:
+    string: str
+    position: int
+
+    def next_char(self):
+        if self.position >= len(self.string):
+            raise EndOfStream()
+        else:
+            self.position += 1
+            return self.string[self.position - 1]
+
+    def unget(self):
+        assert self.position > 0
+        self.position -= 1
+
+
+@dataclass
+class Num:
+    n: int
+
+
+@dataclass
+class Bool:
+    b: bool
+
+
+@dataclass
+class Keyword:
+    word: str
+
+
+@dataclass
+class Identifier:
+    word: str
+
+
+@dataclass
+class Operator:
+    operator: str
+
+
+Token = Num | Bool | Keyword | Identifier | Operator
+
+
+@dataclass
+class EndOfTokens(Exception):
+    pass
+
+
+def convert_to_token(c):
+    if c == "True" or c == "False":
+        return Bool(c)
+    elif c in ["=", ">", "<", "+", "-", "*", "/", "!=", "<=", ">="]:
+        return Operator(c)
+    elif c in "if then else end while do done".split():
+        return Keyword(c)
+    return Identifier(c)
+
+
+@dataclass
+class EndOfToken(Exception):
+    pass
+
+
+@dataclass
+class TokenError(Exception):
+    pass
+
+
+operations = ["=", ">", "<", "+", "-", "*", "/", "!=", "<=", ">="]
+keywords = "if then else end while do done".split()
+
+
+class Lexer:
+    def __init__(self, text):
+        self.text = text
+        self.pos = -1
+        self.current_char = None
+        self.advance()
+
+    def advance(self):
+        self.pos += 1
+        self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
+
+    def backtrack(self):
+        assert self.pos > 0
+        self.pos -= 1
+        self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
+
+    def tokenize(self):
+        tokens = []
+        while self.current_char != None:
+            match self.current_char:
+                case op if op in ["+", "-", "*", "/"]:
+                    tokens.append(Operator(op))
+                    self.advance()
+                case op if op in ["!", "=", ">", "<"]:
+                    self.advance()
+                    if self.current_char == "=":
+                        op += self.current_char
+                        self.advance()
+                    tokens.append(Operator(op))
+                case space if space in " \t\n":
+                    self.advance()
+                case num if num in digit_list:
+                    num_temp = ""
+                    while self.current_char != None and self.current_char in digit_list:
+                        num_temp += self.current_char
+                        self.advance()
+                    tokens.append(Num(int(num_temp)))
+                case s if s in alphabet_list:
+                    temp_str = ""
+                    while (
+                        self.current_char != None
+                        and self.current_char in alphabet_list + digit_list
+                    ):
+                        temp_str += self.current_char
+                        self.advance()
+                    tokens.append(
+                        Keyword(temp_str)
+                    ) if temp_str in keywords else tokens.append(
+                        Bool(temp_str)
+                    ) if temp_str in "True False".split() else tokens.append(
+                        Identifier(temp_str)
+                    )
+                case _:
+                    raise TypeError()
+        return tokens
+
+
+print(Lexer("1 if 1+2==3 then 4 else 5").tokenize())
+
+
 
 
 @dataclass
@@ -47,7 +191,7 @@ class If:
 class BoolLiteral:
     value: bool
 
-    def __init__(self, val:bool):
+    def __init__(self, val: bool):
         self.value = val
 
 
@@ -63,6 +207,73 @@ class Let:
     var: "AST"
     e1: "AST"
     e2: "AST"
+
+
+@dataclass
+class Parser:
+    lexer: Lexer
+
+    def parse_if(self):
+        self.lexer.match("if")
+        c = self.parse_exp()
+        self.lexer.match("then")
+        t = self.parse_exp()
+        self.lexer.match("else")
+        f = self.parse_exp()
+        self.lexer.match("end")
+        return If(c, t, f)
+
+    def parse_atom(self):
+        match self.lexer.peek_token():
+            case Identifier(name):
+                self.lexer.advance()
+                return Variable(name)
+            case Num(value):
+                self.lexer.advance()
+                return NumLiteral(name)
+            case Bool(value):
+                self.lexer.advance()
+                BoolLiteral(value)
+
+    def parse_mul(self):
+        left = self.parse_atom()
+        while True:
+            match self.lexer.peek_token():
+                case Operator(op) if op in "*/":
+                    self.lexer.advance()
+                    m = self.parse_atom()
+                    left = BinOp("*", left, m)
+                case _:
+                    break
+        return left
+
+    def parse_add(self):
+        left = self.parse_mul()
+        while True:
+            match self.lexer.peek_token():
+                case Operator(op) if op in "+-":
+                    self.lexer.advance()
+                    m = self.parse_mul()
+                    left = BinOp("-", left, m)
+                case _:
+                    break
+        return left
+
+    def parse_cmp(self):
+        left = self.parse_add()
+        match self.lexer.peek_token():
+            case Operator(op) if op in "> < >= <= ==".split():
+                self.lexer.advance()
+                m = self.parse_add()
+                left = BinOp(op, left, m)
+        return left
+
+    def parse_expr(self):
+        match self.lexer.peek_token():
+            case Keyword("if"):
+                return self.parse_if()
+            case _:
+                return self.parse_cmp()
 
 
 AST = NumLiteral | BinOp | Variable | Let | BoolLiteral | If
@@ -240,4 +451,56 @@ def test_let_eval():
     e = BinOp("+", Let(a, e1, e2), Let(a, e3, e2))
     assert eval(e) == 22
 
-print(eval(BinOp(">",NumLiteral(3),NumLiteral(5))))
+class Parser:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.current_token = None
+        self.position = -1
+
+    def advance(self):
+        self.position += 1
+        self.current_token = (
+            self.tokens[self.position] if self.position < len(self.tokens) else None
+        )
+
+    def parse_atom(self):
+        match self.current_token:
+            case Num(value):
+                self.advance()
+                return NumLiteral(value)
+            case Identifier(name):
+                self.advance()
+                return Variable(name)
+            case Bool(value):
+                self.advance()
+                return BoolLiteral(value)
+
+    def parse_mul(self):
+        left = self.parse_atom()
+        while self.current_token in "*/":
+            op = self.current_token
+            right = self.parse_atom()
+            left = BinOp(op, left, right)
+        return left
+
+    def parse_add(self):
+        left = self.parse_mul()
+        while self.current_token in "+-":
+            op = self.current_token
+            right = self.parse_mul()
+            left = BinOp(op, left, right)
+        return left
+
+    def parse_bool(self):
+        left = self.parse_add()
+        while self.current_token in "> < >= <= ==".split():
+            op = self.current_token
+            right = self.parse_add()
+            left = BinOp(op, left, right)
+            if self.current_token in "> < >= <= ==".split():
+                raise TypeError()
+        return left
+
+l=Lexer("1").tokenize()
+p=Parser(l).parse_bool()
+print(p)
